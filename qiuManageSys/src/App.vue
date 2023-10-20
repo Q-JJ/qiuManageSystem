7be54bf8f127628c23be5cfa1bc7f8403522d180
Q-JJ <J001222@163.com>
2023-10-20 18:26:57 +0800

    初始化项目

--- qiuManageSys/src/App.vue
+++ qiuManageSys/src/App.vue
@@ -1,30 +1,23 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
+<!--
+ * @Descripttion: 
+ * @version: 
+ * @Author: JnannJ
+ * @Date: 2023-10-20 16:06:56
+ * @LastEditors: JnannJ
+ * @LastEditTime: 2023-10-20 18:12:26
+-->
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-config-provider :locale="zhCn">
+        <router-view />
+    </el-config-provider>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+</script>
+
+<style>
+@import './assets/css/main.css';
+@import './assets/css/color-dark.css';
 </style>
--- qiuManageSys/src/main.ts
+++ qiuManageSys/src/main.ts
@@ -1,5 +1,19 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: JnannJ
+ * @Date: 2023-10-20 16:06:56
+ * @LastEditors: JnannJ
+ * @LastEditTime: 2023-10-20 16:27:51
+ */
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
